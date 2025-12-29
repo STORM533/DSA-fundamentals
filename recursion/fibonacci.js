b21fb88d6f40f0b85621fe61f4c94e565b487991
Saksham Kumar <146557098+STORM533@users.noreply.github.com>
2025-12-30 00:45:05 +0530

    checked for output

--- recursion/fibonacci.js
+++ recursion/fibonacci.js
@@ -16,10 +16,10 @@ const fibsRec = function (input) {
   const arr2 = [0, 1];
   if (input === 1) return arr1;
   if (input === 2) return arr2;
-  
-    const nextValue = fibsRec(input - 1);
-    const ind = nextValue[nextValue.length - 1] + nextValue[nextValue.length - 2];
-    nextValue.push(ind);
-    return nextValue;
-  
+
+  const nextValue = fibsRec(input - 1);
+  const ind = nextValue[nextValue.length - 1] + nextValue[nextValue.length - 2];
+  nextValue.push(ind);
+  return nextValue;
 };
+console.log(fibsRec(8));
