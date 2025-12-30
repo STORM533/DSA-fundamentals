985fedda7fde5703472686cb9e3179590d4914ee
Saksham Kumar <146557098+STORM533@users.noreply.github.com>
2025-12-30 20:58:01 +0530

    complete

--- mergeSort/mergeSort.js
+++ mergeSort/mergeSort.js
@@ -4,17 +4,38 @@ const mergerSort = function (input) {
   if (!Array.isArray(input)) {
     return console.log("Wrong input dawg");
   }
-  if (input.length === 1 || input.length === 0) {
+  if (input.length === 1) {
     return input;
   }
   if (input.length > 1) {
-    mid = Math.floor(input.length / 2);
+    const mid = Math.floor(input.length / 2);
     const left = input.slice(0, mid);
     const right = input.slice(mid);
-    console.log("left:", left);
-    console.log("right:", right);
-    mergerSort(left);
-    mergerSort(right);
+    const input1 = mergerSort(left);
+    const input2 = mergerSort(right);
+    return merge(input1, input2);
   }
 };
-mergerSort([2, 4, 3, 5, 1]);
+
+function merge(input1, input2) {
+  const C = [];
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  while (i < input1.length && j < input2.length) {
+    if (input1[i] < input2[j]) {
+      C[k++] = input1[i++];
+    } else {
+      C[k++] = input2[j++];
+    }
+  }
+  for (; i < input1.length; i++) {
+    C[k++] = input1[i];
+  }
+  for (; j < input2.length; j++) {
+    C[k++] = input2[j];
+  }
+  return C;
+}
+
+console.log(mergerSort([4, 2, 6, 7, 1]));
