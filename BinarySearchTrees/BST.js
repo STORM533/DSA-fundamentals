daddfa03b77d7f532f6643ead999575e1c7f764b
Saksham Kumar <146557098+STORM533@users.noreply.github.com>
2026-02-12 01:43:05 +0530

    deleteItem done

--- BinarySearchTrees/BST.js
+++ BinarySearchTrees/BST.js
@@ -1,7 +1,9 @@
 import { mergerSort } from "../mergeSort/mergeSort.js";
 //every point is a node in tree remember
 //use the node to do anything
-
+//At every node you must answer two questions:
+//Is this where I act?recursion on that node so to go lower or perform action
+//always start from base case then recur down 
 class Node {
     constructor(data = null) {
         this.data = data;
@@ -77,7 +79,73 @@ function Tree(arr) {
         }
     }
     function deleteItem(value) {
-
+        function inOrderSuccessor(node) {
+            if (!node) return null;
+            let current = node;
+        while (current.left) {
+            current = current.left;
+        }
+        let successor = current;
+        return successor;
+        }
+        function search(nodes){
+            if (nodes === null) return;
+            if(value<nodes.data) {
+                if(nodes.left&&nodes.left.data === value) {
+                    if(nodes.left.left === null && nodes.left.right === null){
+                        return nodes.left = null;
+                    }
+                    if(nodes.left.left!==null && nodes.left.right!==null){
+                        let successor =  inOrderSuccessor(nodes.left.right);
+                        let temp = nodes.left.data;
+                        nodes.left.data = successor.data;
+                        successor.data = temp;         
+                        return;         
+                    }
+                    if(nodes.left.left !== null && nodes.left.right === null){
+                        nodes.left = nodes.left.left;
+                        return;
+                    }else {
+                        nodes.left = nodes.left.right;
+                        return; 
+                    }
+                } else {
+                    search(nodes.left);
+                }
+            }else{
+                if(nodes.right&&nodes.right.data===value) {
+                    if(nodes.right.left === null && nodes.right.right === null){
+                        return nodes.right = null;
+                    }
+                    if(nodes.right.right!==null && nodes.right.left !== null){
+                        let successor =  inOrderSuccessor(nodes.right.right);
+                        let temp = nodes.right.data;
+                        nodes.right.data = successor.data;
+                        successor.data = temp; 
+                        return;                 
+                    }
+                    if(nodes.right.left === null && nodes.right.right !== null) {
+                        nodes.right = nodes.right.right;
+                        return; 
+                    }else {
+                        nodes.right = nodes.right.left;
+                        return;
+                    }
+                }else{
+                    search(nodes.right);
+                }                
+            }
+        }
+        if(root.data === value) {
+            if(root.left === null && root.right === null) {
+                root.data = null;
+                root.left = null;
+                root.right = null;
+                return;
+            }
+        }else {
+            search(root);
+        }
     }
     return {
         root,
--- BinarySearchTrees/main.js
+++ BinarySearchTrees/main.js
@@ -1,7 +1,15 @@
 import { Tree } from "./BST.js";
 const test = Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 test.prettyPrint(test.root);
-test.insert( 664);
+console.log("");
+console.log("");
+console.log("");
+console.log("")
+test.deleteItem(67);
+console.log("");
+console.log("");
+console.log("");
+console.log("");
 test.prettyPrint(test.root);
 //pretty print is just a function that [prints] 
 //it does not build the tree [the root which calls buildTree does building]
